b2041927151778f6c342bbda72f865426080578d
Juniper Watson <lyravox1@gmail.com>
2024-04-23 02:03:36 -0500

    Edit information.py

--- bot/cogs/information.py
+++ bot/cogs/information.py
@@ -11,5 +11,18 @@ class Information(commands.Cog):
         latency = int(self.bot.latency * 1000)
         await interaction.response.send_message(f"Pong! My latency is {latency}ms.")
         
+    @nextcord.slash_command(description="Returns a users avatar")
+    async def avatar(self, interaction: Interaction, member: Member = None):
+        if member is None:
+            member = interaction.user
+        avatar = str(member.avatar.url)
+        name = member.name
+        embed = nextcord.Embed(
+            title=f"{name}'s Avatar", 
+            color=0x703c2f
+        )
+        embed.set_image(avatar)
+        await interaction.response.send_message(embed=embed)
+        
 def setup(bot):
     bot.add_cog(Information(bot))
