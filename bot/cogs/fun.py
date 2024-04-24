9ea1daefe093b482d8331a7d2a0d43c03e0cc00f
Juniper Watson <lyravox1@gmail.com>
2024-04-23 23:52:26 -0500

    Edit fun.py

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -4,6 +4,7 @@ from nextcord import Interaction, Member, SlashOption
 import os
 from dotenv import load_dotenv
 import aiohttp
+import random
 
 load_dotenv()
 
@@ -26,18 +27,6 @@ class Fun(commands.Cog):
                     await interaction.response.send_message(embed=embed)
                 else:
                     await interaction.response.send_message("Could not fetch a cat image :(")
-        
-    @nextcord.slash_command(description="Returns a random affirmation")
-    async def affirmation(self, interaction: Interaction):
-        async with aiohttp.ClientSession() as session:
-            async with session.get("https://www.affirmations.dev/") as response:
-                if response.status == 200:
-                    data = await response.json()
-                    affirmation = data['affirmation']
-                    await interaction.response.send_message(f"Affirmation: {affirmation}")
-                else:
-                    await interaction.response.send_message("Could not fetch a affirmation :(")
-                    
                     
     @nextcord.slash_command(description="Returns the astronomy picture of the day (APOD)")
     async def apod(self, interaction: Interaction):
@@ -57,5 +46,33 @@ class Fun(commands.Cog):
                     print(f"Failed to retrive APOD. Status: {response.status}, Error: {error_msg}")
                     await interaction.response.send_message("Failed to retrieve Astronomy Picture of the Day :(")
             
+    @nextcord.slash_command(description="Roll the dice")
+    async def dice(
+        self,
+        interaction: Interaction,
+        sides: int = nextcord.SlashOption(
+            description="Number of sides on the die",
+            required=True,
+            min_value=2,
+            max_value=100
+        ),
+        number: int = nextcord.SlashOption(
+            description="Number of dice to roll",
+            required=True,
+            min_value=1,
+            max_value=200
+        )
+    ):
+        rolls = [random.randint(1, sides) for _ in range(number)]
+        rolls_str = ', '.join(str(roll) for roll in rolls)
+        await interaction.response.send_message(
+            f"Rolled {number} dice with {sides} sides: {rolls_str}"
+        )
+            
+    @nextcord.slash_command(description="Flips a coin")
+    async def coinflip(self, interaction: Interaction):
+        flip_result = random.choice(['heads', 'tails'])
+        await interaction.response.send_message(f"You flipped a coin and got {flip_result}!")
+    
 def setup(bot):
     bot.add_cog(Fun(bot))
