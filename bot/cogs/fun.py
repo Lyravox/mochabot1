e9b2ba84c15671b4486cfd7e491cce731dd8b5a3
Juniper Watson <lyravox1@gmail.com>
2024-04-23 20:16:52 -0500

    Edit fun.py

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -8,6 +8,7 @@ import aiohttp
 load_dotenv()
 
 cat_api = os.getenv("CAT_KEY")
+apod_api = os.getenv("APOD_KEY")
 
 class Fun(commands.Cog):
     def __init__(self, bot):
@@ -36,6 +37,25 @@ class Fun(commands.Cog):
                     await interaction.response.send_message(f"Affirmation: {affirmation}")
                 else:
                     await interaction.response.send_message("Could not fetch a affirmation :(")
-        
+                    
+                    
+    @nextcord.slash_command(description="Returns the astronomy picture of the day (APOD)")
+    async def apod(self, interaction: Interaction):
+        apod_url = "https://api.nasa.gov/planetary/apod"
+        async with aiohttp.ClientSession() as session:
+            params = {'api_key': apod_api}
+            async with session.get(apod_url, params=params) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    image_url = data.get("url")
+                    explanation = data.get("explanation")
+                    embed = nextcord.Embed(title="Astronomy Picture of the Day", color=0x703c2f, description=explanation)
+                    embed.set_image(url=image_url)
+                    await interaction.response.send_message(embed=embed)
+                else:
+                    error_msg = await response.text()
+                    print(f"Failed to retrive APOD. Status: {response.status}, Error: {error_msg}")
+                    await interaction.response.send_message("Failed to retrieve Astronomy Picture of the Day :(")
+            
 def setup(bot):
     bot.add_cog(Fun(bot))
